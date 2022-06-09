52771c5aba2424cd473dab35b69938a1890c957d
awdriggs <awdriggs@gmail.com>
2022-06-09 03:51:19 +0000

    working on sieve

---
+++ pre07/Seive.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Sieve {
+// Write a method called sieve that takes an integer parameter, n, and returns a boolean array that indicates, for each number from 0 to n - 1, whether the number is prime.
+  public static boolean[] sieve(int n){
+    boolean[] primes = new boolean[n-2];
+    
+  }
+  
+  public static void main(String[] args){
+  
+  }
+}
